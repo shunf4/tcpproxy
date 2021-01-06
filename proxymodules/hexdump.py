49badd775d1d9ff71a1c65eeccb2b62bdefb3f78
shunf4 <shun1048576@gmail.com>
2021-01-06 13:29:46 +0800

    feat: add wireshark direction indication to hexdump

--- proxymodules/hexdump.py
+++ proxymodules/hexdump.py
@@ -9,23 +9,31 @@ class Module:
         self.description = 'Print a hexdump of the received data'
         self.incoming = incoming  # incoming means module is on -im chain
         self.len = 16
+        self.wsdirection = False
         if options is not None:
             if 'length' in options.keys():
                 self.len = int(options['length'])
+            if str(options.keys().get("wsdirection", "0")) == "1":
+                self.wsdirection = True
 
     def help(self):
-        return '\tlength: bytes per line (int)'
+        return '\tlength: bytes per line (int)\n\twsdirection: set to 1 to enable wireshark direction indication'
 
     def execute(self, data):
+        result = []
+        
+        if self.wsdirection:
+            result.append("I" if self.incoming else "O")
+
         # this is a pretty hex dumping function directly taken from
         # http://code.activestate.com/recipes/142812-hex-dumper/
-        result = []
+        
         digits = 2
         for i in range(0, len(data), self.len):
             s = data[i:i + self.len]
             hexa = ' '.join(['%0*X' % (digits, x) for x in s])
             text = ''.join([chr(x) if 0x20 <= x < 0x7F else '.' for x in s])
-            result.append("%04X   %-*s   %s" % (i, self.len * (digits + 1), hexa, text))
+            result.append("%08X   %-*s   %s" % (i, self.len * (digits + 1), hexa, text))
         print("\n".join(result))
         return data
 
